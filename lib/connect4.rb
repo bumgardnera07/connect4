0dff7679c496bea52d8f4b78b5ea189995564674
George Bumgardner <bumgardnera07@gmail.com>
2017-12-28 00:36:53 +0000

    building ruby file

---
+++ lib/connect4.rb
@@ -0,0 +1,18 @@
+module Connect4
+    class Board
+        attr_reader :rows, :columns, :cells
+        
+        #Build a blank board. Standard Connect4 Board is 6 rows, 7 columns
+        
+        def initialize(rows=6, columns=7)
+            @rows, @columns = rows, columns
+            @cells = columns.times.map { [] }
+        end
+        
+        def print_board
+            board = "+#{'---+'*@columns}\n"
+            board << "|#{@columns.times.map{|p|"#{(p+1).to_s.center(3)}|"}.join\n"
+            board << "+#{'---+'*@columns}\n"
+            board
+        end
+        
